1df8b64aed7eaa24b55db656696df4a764d17461
Jacob Hayslett <jhayslett@IDTD25KV1VHDNMPW.local>
2017-12-08 15:33:29 -0700

    Add first commit

---
+++ js/homepage.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+	var links = [
+		["SurveyGizmo Application","https://app.surveygizmo.com/login/v1"],
+		["Knowledge Center - Production","http://portalprd.discounttire.com/portal/server.pt"]
+	];
+
+	var html = "";
+	var linkArea = document.querySelector(".linksBody");
+	html += "<ul>";
+	for(var i = 0; i < links.length; i++) {
+		html += "<li><a target='_blank' href='" + links[i][1] + "'>" + links[i][0] + "</a></li>";
+	}
+	html += "</ul>";
+	linkArea.innerHTML = html;
+
+});
