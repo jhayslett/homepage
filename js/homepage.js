95a9668099bf5047c02acea19e10611dbf73253a
jhayslett <jhayslett@gmail.com>
2018-01-03 21:59:34 -0700

    Added more links and classes to the UL

--- js/homepage.js
+++ js/homepage.js
@@ -1,19 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(){
 
 	var links = [
-		["SurveyGizmo Application","https://app.surveygizmo.com/login/v1"],
-		["Knowledge Center - Production","http://portalprd.discounttire.com/portal/server.pt"],
-		["Knowledge Center - Staging","http://portalprd.discounttire.com/portal/server.pt"],
-		["Amazon Music","https://music.amazon.com/home"],
-		["Edit Homepage JS","https://github.com/jhayslett/homepage/blob/master/js/homepage.js"],
-		["SlickDeals","https://slickdeals.net"]
+		["SurveyGizmo Application","https://app.surveygizmo.com/login/v1","Work"],
+		["Knowledge Center - Production","http://portalprd.discounttire.com/portal/server.pt","Work"],
+		["Knowledge Center - Staging","http://portalprd.discounttire.com/portal/server.pt","Work"],
+		["Amazon Music","https://music.amazon.com/home","Personal"],
+		["Edit Homepage JS","https://github.com/jhayslett/homepage/blob/master/js/homepage.js","Classwork"],
+		["SlickDeals","https://slickdeals.net","Personal"],
+		["Modern Developer - Study","http://study.moderndeveloper.com","Classwork"],
+		["Modern Developer - QA","http://qa.moderndeveloper.com","Classwork"],
+		["CodePen","https://codepen.io/dashboard/","Classwork"],
+		["Github","https://github.com","Classwork"],
+		["GitHub - Coursework","https://github.com/moderndeveloper-students/coursework-jhayslett","Classwork"]
 	];
 
 	var html = "";
 	var linkArea = document.querySelector(".linksBody");
 	html += "<ul>";
+
+	// CREATE INDEX OF HEADERS
+
+
 	for(var i = 0; i < links.length; i++) {
-		html += "<li><a target='_blank' href='" + links[i][1] + "'>" + links[i][0] + "</a></li>";
+		html += "<li class='" + links[i][2] + "'><a target='_blank' href='" + links[i][1] + "'>" + links[i][0] + "</a></li>";
 	}
 	html += "</ul>";
 	linkArea.innerHTML = html;
